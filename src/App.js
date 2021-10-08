93adfba5736f615c18bb0be25ad6cfc5834df560
kenhyj <jhy_1993@hotmail.com>
2021-10-08 12:44:49 -0700

    some ugly working switch route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Logo from "./components/logo/logo";
+import Sector from "./pages/Sector/Sector";
+import Resume from "./pages/Resume/Resume";
+import Proposal from "./pages/Proposal/Proposal";
 
 function App() {
   return (
@@ -13,8 +16,36 @@ function App() {
       <div className="mainstage">
         <p>Resume Generator</p>
       </div>
-      <Login />
-      <Home />
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+      <Sector/>
+      <Proposal />
     </div>
   );
 }
--- src/pages/Sector/Sector.jsx
+++ src/pages/Sector/Sector.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Sector(){
     return (
         <div>
-            Sector
+            Create New Sector
         </div>
     )
 }
