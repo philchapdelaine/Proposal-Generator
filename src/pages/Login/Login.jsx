62dd04dae4cd704e242a96a10723fa530d997a5c
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2021-10-07 21:24:06 -0700

    fixed some import issues with MUI icons

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import "./Login.css";
 
+import LockIcon from "@mui/icons-material/Lock";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 function Login() {
   return (
     <div className="Login">
       Login
-      <form>
-        <label>
-          Username:
-          <input type="text" name="username" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <LoginBox />
     </div>
+  );
+}
+
+function LoginBox() {
+  return (
+    <form>
+      <label>
+        Username:
+        <AccountCircleIcon />
+        <input type="text" name="username" />
+      </label>
+      <br />
+      <label>
+        Password:
+        <LockIcon />
+        <input type="password" name="password" />
+      </label>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
