23024d36a75f282eaca3a2789b7aeb8cbcf0a25e
kenhyj <jhy_1993@hotmail.com>
2021-10-07 20:27:52 -0700

    pages added components dir added. some unnecessary file deleted

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 // import logo from "./logo.svg";
 import logo from "./assets/ae-300x132.jpg";
-import "./App.css";
+// import "./App.css";
+// import "./home.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={logo} />
+        <img src={logo} className="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> Resume Generator
       </header>
     </div>
   );
 }
 
 export default App;
+
+// #00569c the blue color
