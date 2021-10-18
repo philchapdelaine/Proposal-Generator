f22f52eb8c5f8ca28514c30f72b169070292c002
irkules <meganempon@gmail.com>
2021-10-17 17:23:57 -0700

    add buttons to navigator bar

--- src/components/navigator_bar/NavigatorBar.jsx
+++ src/components/navigator_bar/NavigatorBar.jsx
@@ -5,9 +5,10 @@ function NavigatorBar(){
     return (
         <div className="nav-bar" >
             <button className="nav-button"> Resume </button>
+            <button className="nav-button"> Admin </button>
             <button className="nav-button"> Create Resume Proposal </button>
+            <button className="nav-button"> Create New Sector </button>
             <div className="recently-viewed"> Recently viewed component (placeholder) </div>
-            <div className="search-sector"> Search sector componenet (placeholder) </div>
         </div>
         
     )
