5295a2956ea32c5e1400b09fef25bd987ab80a00
irkules <meganempon@gmail.com>
2021-11-10 23:00:14 -0800

    add & import dummy data from CreateProposal to RecentlyViewed, show RecentlyViewed only on CreateProposal page

--- src/pages/Proposal/CreateProposal.jsx
+++ src/pages/Proposal/CreateProposal.jsx
@@ -1,34 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import ReadingPane from "../../components/reading_pane/ReadingPane";
-import ResumeThumbnail from "../../components/resume_thumbnail/ResumeThumbnail";
 import NavigatorBar from "../../components/navigator_bar/NavigatorBar";
 import ResumeSectorDisplay from "../../components/resume_sector_display/ResumeSectorDisplay";
 import "./CreateProposal.css";
 import TextField from "@material-ui/core/TextField";
 
-const dummyResumes = [
-  {
-    name: "first last",
-  },
-  {
-    name: "first last",
-  },
-  {
-    name: "first last",
-  },
-];
 
-const resumes = [];
+// dummy data; this will actually come from the search api
+const recentlyViewedSample = [
+  {
+    "resumeID": 1,
+    "sectors": [
+        {
+            "sectorID": 1,
+            "name": "Experience",
+            "linkedEmail": "mc@ae.com",
+            "fileType": "txt",
+            "division": "Water",
+            "imageLoc": "blah/blah",
+            "description": "I'm the best so I don't need to have any experience"
+        },
+        {
+            "sectorID": 2,
+            "name": "Projects",
+            "linkedEmail": "mc@ae.com",
+            "fileType": "txt",
+            "division": "Air",
+            "imageLoc": null,
+            "description": "I'm the best so I don't need to have any projects"
+        }
+    ]
+  },
+  {
+    "resumeID": 2,
+    "sectors": [
+        {
+            "sectorID": 1,
+            "name": "Experience",
+            "linkedEmail": "mc@ae.com",
+            "fileType": "txt",
+            "division": "Water",
+            "imageLoc": "blah/blah",
+            "description": "I'm the best so I don't need to have any experience"
+        },
+        {
+            "sectorID": 2,
+            "name": "Projects",
+            "linkedEmail": "mc@ae.com",
+            "fileType": "txt",
+            "division": "Air",
+            "imageLoc": null,
+            "description": "I'm the best so I don't need to have any projects"
+        }
+    ]
+  },
+]
 
 function CreateProposal() {
-  for (const resume of dummyResumes) {
-    resumes.push(
-      <ResumeThumbnail name={resume["name"]} addable={true}></ResumeThumbnail>
-    );
-  }
   return (
     <div className="create-proposal">
-      <NavigatorBar />
+      <NavigatorBar isCreateProposal={true} recentlyViewed={recentlyViewedSample}></NavigatorBar>
       <div className="cp-center-pane">
         <div className="cp-center-header">
           <div className="title"> Create Proposal </div>
