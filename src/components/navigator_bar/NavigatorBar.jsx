b564fc1064df4bb0e03148454ea06dfd8418b236
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2021-11-18 00:18:45 -0800

    redux restored on admin status in NavigatorBar jsx

--- src/components/navigator_bar/NavigatorBar.jsx
+++ src/components/navigator_bar/NavigatorBar.jsx
@@ -3,7 +3,6 @@ import RecentlyViewed from "../../components/recently_viewed/RecentlyViewed";
 import "./NavigatorBar.css";
 import { useSelector, useDispatch } from "react-redux";
 
-
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,16 +12,16 @@ import {
 } from "react-router-dom";
 
 function NavigatorBar(props) {
-    // delete this isAdmin and replace with the other one after testing
-    const isAdmin = true;
-    //const isAdmin = useSelector((state) => state.loginReducer.admin);
-    const dispatch = useDispatch();
+  // delete this isAdmin and replace with the other one after testing
+  // const isAdmin = true;
+  const isAdmin = useSelector((state) => state.loginReducer.admin);
+  const dispatch = useDispatch();
 
-    function handleCreateProposal() {
-        // set to the as of yet uncreated proposal index flag of -1, 
-        // which will be changed when proposal is actually created and has an index
-        dispatch({ type: 'SET_PROPOSAL_INDEX', currentProposalIndex: -1 });
-    }
+  function handleCreateProposal() {
+    // set to the as of yet uncreated proposal index flag of -1,
+    // which will be changed when proposal is actually created and has an index
+    dispatch({ type: "SET_PROPOSAL_INDEX", currentProposalIndex: -1 });
+  }
 
   return (
     <div className="nav-bar">
@@ -31,24 +30,37 @@ function NavigatorBar(props) {
           <button className="nav-button"> Resume </button>
         </Link>
         {isAdmin ? (
-        <Link to="/admin" style={{ textDecoration: "none" }}>
-          <button className="nav-button"> Admin </button>
-        </Link>
+          <Link to="/admin" style={{ textDecoration: "none" }}>
+            <button className="nav-button"> Admin </button>
+          </Link>
         ) : null}
         {isAdmin ? (
-        <Link to="/create-proposal" style={{ textDecoration: "none" }}>
-          <button className="nav-button" onClick={() => handleCreateProposal()}> Create Resume Proposal </button>
-        </Link>
+          <Link to="/create-proposal" style={{ textDecoration: "none" }}>
+            <button
+              className="nav-button"
+              onClick={() => handleCreateProposal()}
+            >
+              {" "}
+              Create Resume Proposal{" "}
+            </button>
+          </Link>
         ) : null}
         {isAdmin ? (
-        <Link to="/sector" style={{ textDecoration: "none" }}>
-          <button className="nav-button"> Create New Sector </button>
-        </Link>
+          <Link to="/sector" style={{ textDecoration: "none" }}>
+            <button className="nav-button"> Create New Sector </button>
+          </Link>
         ) : null}
       </nav>
       <div className="recently-viewed">
         {" "}
-        {props.isCreateProposal ? <RecentlyViewed onSectorClick={props.onSectorClick} resumes={props.recentlyViewed}>{props.recentlyViewed}</RecentlyViewed> : null }
+        {props.isCreateProposal ? (
+          <RecentlyViewed
+            onSectorClick={props.onSectorClick}
+            resumes={props.recentlyViewed}
+          >
+            {props.recentlyViewed}
+          </RecentlyViewed>
+        ) : null}
       </div>
     </div>
   );
