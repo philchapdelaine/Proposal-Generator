aad4e466a0128217d133ba2723c5170058cb3431
irkules <meganempon@gmail.com>
2021-11-20 14:39:05 -0800

    actually fix name retrieval in ResumeSectorDisplay

--- src/components/resume_sector_display/ResumeSectorDisplay.jsx
+++ src/components/resume_sector_display/ResumeSectorDisplay.jsx
@@ -6,110 +6,107 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-import TableBody from "@mui/material/TableBody";
 import { makeStyles } from '@material-ui/core/styles';
 import "./ResumeSectorDisplay.css";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 
 const useStyles = makeStyles(() => ({
-    resumeOwnerInfo: {
-        marginLeft: "15px",
-        marginRight: "10px"
-    },
+  resumeOwnerInfo: {
+    marginLeft: "15px",
+    marginRight: "10px"
+  },
 }));
 
 
 
 export default function ResumeSectorDisplay(props) {
-    const classes = useStyles();
-    const [expanded, setExpanded] = useState(false);
-    // const [resumeOwnerName, setResumeOwnerName] = useState("");
-    // const [resumeOwnerEmail, setResumeOwnerEmail] = useState("");
-    const dispatch = useDispatch();
+  const [expanded, setExpanded] = useState(false);
+  const dispatch = useDispatch();
 
-    function handleSectorClick(sector, currResume) {
-        dispatch({ type: 'SET_CURRENT_SECTOR', currentSector: sector })
-        props.onSectorClick(sector, currResume);
-    }
+  function handleSectorClick(sector, currResume) {
+    dispatch({ type: 'SET_CURRENT_SECTOR', currentSector: sector })
+    props.onSectorClick(sector, currResume);
+  }
 
-    function generateRows(sector, currResume) {
-        return (
-            <TableRow hover onClick={() => handleSectorClick(sector, currResume)}>
-              <TableCell sx={{ width: "13%" }} align="left">
-                {sector.name}
-              </TableCell>
-              <TableCell sx={{ width: "13%" }} align="left">
-                {sector.linkedEmail}
-              </TableCell>
-              <TableCell sx={{ width: "13%" }} align="left">
-                {"Proposal # " + sector.proposalNumber}
-              </TableCell>
-              <TableCell sx={{ width: "13%" }} align="left">
-                {sector.division}
-              </TableCell>
-              <TableCell sx={{ width: "13%" }} align="left">
-                {sector.imageLoc}
-              </TableCell>
-              <TableCell sx={{ width: "33%" }} align="left">
-                {sector.description}
-              </TableCell>
-            </TableRow>
-        );
-    }
+  function generateRows(sector, currResume) {
+    return (
+      <TableRow hover onClick={() => handleSectorClick(sector, currResume)}>
+        <TableCell sx={{ width: "13%" }} align="left">
+          {sector.name}
+        </TableCell>
+        <TableCell sx={{ width: "13%" }} align="left">
+          {sector.linkedEmail}
+        </TableCell>
+        <TableCell sx={{ width: "13%" }} align="left">
+          {"Proposal # " + sector.proposalNumber}
+        </TableCell>
+        <TableCell sx={{ width: "13%" }} align="left">
+          {sector.division}
+        </TableCell>
+        <TableCell sx={{ width: "13%" }} align="left">
+          {sector.imageLoc}
+        </TableCell>
+        <TableCell sx={{ width: "33%" }} align="left">
+          {sector.description}
+        </TableCell>
+      </TableRow>
+    );
+  }
 
-    // useEffect(() => {
-    //     getFeedback();
-    // }, []);
+  function generateAccordian(resume) {
+    return (
+      <Accordion
+        expanded={expanded === "panel" + resume.resumeID}
+        onChange={handleChange("panel" + resume.resumeID)}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon className="rsd-expand-icon" />}
+          aria-controls={"panel" + resume.resumeID + " bh-content"}
+          id={"panel" + resume.resumeID + "bh-header"}
+          sx={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}
+        >
+          <ResumeOwnerDisplay ownerID={resume.resumeID}></ResumeOwnerDisplay>
+        </AccordionSummary>
+        <AccordionDetails>
+          {resume.sectors.map((sector) => generateRows(sector, resume))}
+        </AccordionDetails>
+      </Accordion>
+    );
+  }
 
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
-    function getResumeOwnerInfo(resumeID) {
-       // var uid = useSelector((state) => state.loginReducer.uid);
-        var url = `/api/user/${resumeID}/`
-        var infoArray = [];
-        axios.get(url)
-            .then((res) => {
-                // setResumeOwnerName(res.data.firstName + " " + res.data.lastName);
-                // setResumeOwnerEmail(res.data.emailAddress);
-                infoArray.push(res.data.firstName + " " + res.data.lastName);
-                infoArray.push(res.data.emailAddress);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-        return infoArray;
-    }
+  return props.searchedResumes.map(generateAccordian);
+}
 
-    function generateAccordian(resume) {
-        return (
-            <Accordion
-                expanded={expanded === "panel" + resume.resumeID}
-                onChange={handleChange("panel" + resume.resumeID)}
-            >
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon className="rsd-expand-icon" />}
-                    aria-controls={"panel" + resume.resumeID + " bh-content"}
-                    id={"panel" + resume.resumeID + "bh-header"}
-                    sx={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}
-                >
-                    <Typography className={classes.resumeOwnerInfo} sx={{ width: "33%", flexShrink: 0 }}>
-                        { getResumeOwnerInfo(resume.resumeID)[0] || "Name not available"}
-                    </Typography>
-                    <Typography sx={{ color: "text.secondary" }}>
-                        { getResumeOwnerInfo(resume.resumeID)[1] || " "}
-                    </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    {resume.sectors.map((sector) => generateRows(sector, resume))}
-                </AccordionDetails>
-            </Accordion>
-        );
-    }
+export function ResumeOwnerDisplay(props) {
+  const [resumeOwnerName, setResumeOwnerName] = useState("");
+  const [resumeOwnerEmail, setResumeOwnerEmail] = useState("");
+  const classes = useStyles();
 
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
-
-    return props.searchedResumes.map(generateAccordian);
+  useEffect(() => {
+    axios
+      .get("/api/user/" + props.ownerID + "/")
+      .then((res) => {
+        setResumeOwnerName(res.data.firstName + " " + res.data.lastName);
+        setResumeOwnerEmail(res.data.emailAddress);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  return (
+    <>
+      <Typography className={classes.resumeOwnerInfo} sx={{ width: "33%", flexShrink: 0 }}>
+        {resumeOwnerName || "Name not available"}
+      </Typography>
+      <Typography sx={{ color: "text.secondary" }}>
+        {resumeOwnerEmail || " "}
+      </Typography>
+    </>
+  )
 }
