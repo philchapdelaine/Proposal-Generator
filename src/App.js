b8f11f2e613684d31211c5423a7500eab9e582b7
kenhyj <mathkenjeong@gmail.com>
2021-11-21 00:02:43 -0800

    redirect fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import UserDropdown from "./components/user_dropdown/UserDropdown";
 
 import MenuIcon from "@mui/icons-material/Menu";
 
-import Navigator from "./components/navigator/Navigator";
+// import Navigator from "./components/navigator/Navigator";
 import { useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
@@ -14,8 +14,36 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import React from "react";
+
+import Login from "./pages/Login/Login";
+import CreateSector from "./pages/Sector/CreateSector";
+import Resume from "./pages/Resume/Resume";
+import Proposal from "./pages/Proposal/Proposal";
+import CreateProposal from "./pages/Proposal/CreateProposal";
+import Signup from "./pages/Signup/Signup";
+import Admin from "./pages/Admin/Admin";
+
+/* TODO: redirect to login if the user is not logged in.
+ */
+
 function App() {
   const isLoggedIn = useSelector((state) => state.loginReducer.loggedIn);
+  const imadmin = useSelector((state) => state.loginReducer["admin"]);
+
+  const loggedinRedirects = () => {
+    if (isLoggedIn) {
+      if (imadmin) {
+        // return <Redirect to="/admin" />;
+        return <Admin/>;
+      } else {
+        // return <Redirect to="/resume" />;
+        return <Resume/>;
+      }
+    } else {
+      return <Login />;
+    }
+  };
 
   return (
     <div>
@@ -24,9 +52,25 @@ function App() {
           <Logo />
           <span className="resume-generator">Resume Generator</span>
           {isLoggedIn ? <UserDropdown /> : <Redirect to="/login" />}
+          {/* {isLoggedIn ? <UserDropdown /> : null} */}
         </header>
         <br />
-        <Navigator />
+        {isLoggedIn ? (
+            <Switch>
+              <Route path="/login" component={Login}/>
+              <Route path="/admin" component={Admin}/>
+              <Route path="/resume" component={Resume}/>
+              <Route path="/sector" component={CreateSector}/>
+              <Route path="/create-proposal" component={CreateProposal}/>
+              <Route path="/">{loggedinRedirects}</Route>
+            </Switch>
+          ) : (
+            <Switch>
+              <Route path="/login" component={Login}/>
+              <Route path="/signup" component={Signup}/>
+              <Route path="/">{loggedinRedirects}</Route>
+            </Switch>
+          )}
       </Router>
     </div>
   );
