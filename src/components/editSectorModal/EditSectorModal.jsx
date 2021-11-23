b861f1af566945fa050653fbd69601f6396eea1a
irkules <meganempon@gmail.com>
2021-11-23 13:59:38 -0800

    possible fix for edit sector modal, still has bugs

--- src/components/editSectorModal/EditSectorModal.jsx
+++ src/components/editSectorModal/EditSectorModal.jsx
@@ -1,6 +1,6 @@
-import { Button, Modal, TextField, Typography } from "@mui/material";
+import { Button, Modal, TextField, Typography, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCurrentSector } from "../../redux/actions/proposal-actions";
@@ -25,55 +25,29 @@ const buttonStyle = {
 
 function EditSectorModal(props) {
   const { sectorID, name, description, division, imageLoc, linkedEmail } = props.sector;
-
-  const [newName, setNewName] = useState(name);
-  const [newDescription, setNewDescription] = useState(description);
-  const [newDivision, setNewDivision] = useState(division);
-  const [newImageLoc, setNewImageLoc] = useState(imageLoc);
-  const [newLinkedEmail, setNewLinkedEmail] = useState(linkedEmail);
+  const [newSector, setNewSector] = useState(props.sector); 
 
   const dispatch = useDispatch();
 
   const handleSave = async () => {
     if (validateEmail()) {
-      const updatedSector = {
-        sectorID: sectorID,
-        name: newName,
-        description: newDescription,
-        division: newDivision,
-        imageLoc: newImageLoc,
-        linkedEmail: newLinkedEmail,
-      }
       // sets current sector to the updated sector. DOES NOT update anything in db, nor does it affect the original sector
-      dispatch(setCurrentSector(updatedSector));
+      dispatch(setCurrentSector(newSector));
       props.onClose();
-    }
+      }
   };
+  useEffect( () => {
+  }, [newSector])
+ 
 
-  const onTextChange = (e, label) => {
-    switch (label) {
-      case "Name":
-        setNewName(e.target.value);
-        break;
-      case "Description":
-        setNewDescription(e.target.value);
-        break;
-      case "Division":
-        setNewDivision(e.target.value);
-        break;
-      case "ImageLoc":
-        setNewImageLoc(e.target.value);
-        break;
-      case "LinkedEmail":
-        setNewLinkedEmail(e.target.value);
-        break;
-    }
+  const onTextChange = e => {
+    setNewSector({ ...newSector, [e.target.name]: e.target.value });
   };
 
   const validateEmail = () => {
-    if (!newLinkedEmail) {
+    if (!newSector.linkedEmail) {
       return false;
-    } else if (!isEmail(newLinkedEmail)) {
+    } else if (!isEmail(newSector.linkedEmail)) {
       return false;
     } else return true;
   };
@@ -88,18 +62,50 @@ function EditSectorModal(props) {
             Edit sector
           </Typography>
           <Box display="flex" flexDirection="column">
-            <TextField label="Name" size="small" defaultValue={name} onChange={(e) => onTextChange(e, "Name")} style={{marginBottom: 10}}/>
-            <TextField label="Division" size="small" defaultValue={division} onChange={(e) => onTextChange(e, "Division")} style={{marginBottom: 10}}/>
+            <FormControl fullWidth>
+              <InputLabel htmlFor="uncontrolled-native">Type</InputLabel>
+              <Select
+                label="Type"
+                name="name"
+                defaultValue={name}
+                onChange={onTextChange}
+                sx={{ marginBottom: "10px" }}
+              >
+                <MenuItem className="cs-menuitem" value="Role">Role</MenuItem>
+                <MenuItem className="cs-menuitem" value="Education">Education</MenuItem>
+                <MenuItem className="cs-menuitem" value="Experience">Experience</MenuItem>
+                <MenuItem className="cs-menuitem" value="Summary">Summary</MenuItem>
+                <MenuItem className="cs-menuitem" value="Justification">Justification</MenuItem>
+                <MenuItem className="cs-menuitem" value="Publications">Publications</MenuItem>
+              </Select>
+            </FormControl>
+            {/* <TextField label="Division" name="division" size="small" defaultValue={division} onChange={(e) => onTextChange(e)} style={{marginBottom: 10}}/> */}
+            <FormControl fullWidth>
+              <InputLabel htmlFor="uncontrolled-native">Division</InputLabel>
+              <Select
+                label="Division"
+                name="division"
+                defaultValue={division}
+                onChange={onTextChange}
+                sx={{ marginBottom: "10px" }}
+              >
+                <MenuItem className="cs-menuitem" value="Water">Water</MenuItem>
+                <MenuItem className="cs-menuitem" value="Bridge">Bridge</MenuItem>
+                <MenuItem className="cs-menuitem" value="Electrical">Electrical</MenuItem>
+                <MenuItem className="cs-menuitem" value="Environmental">Environmental</MenuItem>
+                <MenuItem className="cs-menuitem" value="Civil">Civil</MenuItem>
+              </Select>
+            </FormControl>
             <TextField 
-              label="Employee email" size="small"
+              label="Employee email" size="small" name="linkedEmail"
               defaultValue={linkedEmail}  
-              onChange={(e) => onTextChange(e, "LinkedEmail")} 
+              onChange={onTextChange} 
               error={!validateEmail()} 
               helperText={"Employee email must be a valid email format"} 
               style={{marginBottom: 10}}
             />
-            <TextField label="Image location" size="small" defaultValue={imageLoc} onChange={(e) => onTextChange(e, "ImageLoc")} style={{marginBottom: 10}}/>
-            <TextField label="Description" size="small" defaultValue={description} onChange={(e) => onTextChange(e, "Description")} multiline maxRows={4}/>
+            <TextField label="Image location" name="imageLoc" size="small" defaultValue={imageLoc} onChange={onTextChange} style={{marginBottom: 10}}/>
+            <TextField label="Description" name="description" size="small" defaultValue={description} onChange={onTextChange} multiline maxRows={4}/>
           </Box>
           <Box display="flex" justifyContent="center" style={{marginTop: 10}}>
             <Button style={buttonStyle} variant="outlined" onClick={props.onClose}>Cancel</Button>
