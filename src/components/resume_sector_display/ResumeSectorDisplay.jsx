b59e6a09fa242a12c537c2fe6047a4bfb2331ed4
ricky <rickyli1996@gmail.com>
2021-11-24 20:47:15 -0800

    added table headers for the sectors, want to add a scrollbar for inside accordian and outside

--- src/components/resume_sector_display/ResumeSectorDisplay.jsx
+++ src/components/resume_sector_display/ResumeSectorDisplay.jsx
@@ -4,8 +4,11 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
+import TableHead from '@mui/material/TableHead';
 import { makeStyles } from '@material-ui/core/styles';
 import "./ResumeSectorDisplay.css";
 import axios from "axios";
@@ -70,7 +73,25 @@ export default function ResumeSectorDisplay(props) {
           <ResumeOwnerDisplay ownerID={resume.resumeID}></ResumeOwnerDisplay>
         </AccordionSummary>
         <AccordionDetails>
+          {/* {resume.sectors.map((sector) => generateRows(sector, resume))} */
+          
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Type</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Proposal Number</TableCell>
+            <TableCell>Division</TableCell>
+            <TableCell>Image Location</TableCell>
+            <TableCell>Description </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {resume.sectors.map((sector) => generateRows(sector, resume))}
+        </TableBody>
+      </Table>
+          
+          }
         </AccordionDetails>
       </Accordion>
     );
